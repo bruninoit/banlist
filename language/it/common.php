6376834ad5904365eb3daf9668197d8e2845b27d
bruninoit <brunino98@hotmail.it>
2014-11-17 14:33:42 +0100

    a2

--- language/it/common.php
+++ language/it/common.php
@@ -1,19 +1,14 @@
 <?php
-/**
-*
-* @package phpBB Extension - Acme Demo
-* @copyright (c) 2013 phpBB Group
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
-*
-*/
 if (!defined('IN_PHPBB'))
 {
 	exit;
 }
+
 if (empty($lang) || !is_array($lang))
 {
 	$lang = array();
 }
+
 $lang = array_merge($lang, array(
 	'BANNED_NAME'			=> 'Nickname',
 	'BAN_LIST'			=> 'Lista Ban',
@@ -22,6 +17,6 @@ $lang = array_merge($lang, array(
 	'BAN_REASON'			=> 'Motivo',
     'PERMABAN'           => 'Ban Permanente',
     'NO_REASON_BAN'      => 'Nessuna motivazione specificata',
-    'TOTAL_BAN'          => 'Un utente bannato',
-    'TOTAL_BANS'          => '%s utenti bannati',
+    'TOTALS_BAN'          => 'C&apos;&egrave; un utente bannato',
+    'TOTAL_BANS'          => 'Ci sono %s utenti bannati',
 ));
